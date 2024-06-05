4d2e8c7d8923c16e78005fda01e32ae68604db2c
thomazf <thomaz.fneves@gmail.com>
2024-06-05 00:29:39 -0300

    criação de template html e de controller para acesso

---
+++ src/main/java/com/possoler/respondeai/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.possoler.respondeai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@CrossOrigin("*")
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+}
