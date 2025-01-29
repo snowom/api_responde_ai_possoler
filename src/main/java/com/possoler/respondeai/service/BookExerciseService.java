4b1664d07354aba8f85b426b7f03ca2d42f98f87
thomazf <thomaz.fneves@gmail.com>
2025-01-28 23:23:14 -0300

    create json helper class, and refactoring BookExerciseService class

--- src/main/java/com/possoler/respondeai/service/BookExerciseService.java
+++ src/main/java/com/possoler/respondeai/service/BookExerciseService.java
@@ -3,6 +3,7 @@ package com.possoler.respondeai.service;
 import com.possoler.respondeai.dto.response.BookExerciseResponseDTO;
 import com.possoler.respondeai.exceptions.ServerErrorException;
 import com.possoler.respondeai.interfaces.RespondeAiClient;
+import com.possoler.respondeai.utils.json.JsonHelper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,8 +16,13 @@ import java.util.List;
 public class BookExerciseService {
 
     private final RespondeAiClient respondeAiClient;
+    private final JsonHelper jsonHelper;
 
-    public BookExerciseService(@Qualifier("BookExerciseClient") RespondeAiClient respondeAiClient) {
+    public BookExerciseService(
+        @Qualifier("BookExerciseClient") RespondeAiClient respondeAiClient,
+        JsonHelper jsonHelper
+    ) {
+        this.jsonHelper = jsonHelper;
         this.respondeAiClient = respondeAiClient;
     }
 
@@ -27,45 +33,14 @@ public class BookExerciseService {
 
     private BookExerciseResponseDTO buildBookResponse(String responseBody) {
         JSONObject jsonObject = new JSONObject(responseBody);
-
-        var lightSolution = buildLightSolutionResponse(jsonObject);
-        var lightBody = buildLightBodyResponse(jsonObject);
-        var lightAnswer = buildLightAnswerResponse(jsonObject);
+        List<String> lightSolution = jsonHelper.getArrayObject(jsonObject, "lightSolution");
+        String lightBody = jsonHelper.getObject(jsonObject, "lightBody");
+        String lightAnswer = jsonHelper.getObject(jsonObject, "lightAnswer");
 
         return BookExerciseResponseDTO.builder()
             .lightBody(lightBody)
             .lightAnswer(lightAnswer)
             .lightSolution(lightSolution)
             .build();
-    }
-
-    private String buildLightBodyResponse(JSONObject jsonObject) {
-        try{
-            return jsonObject.get("lightBody").toString();
-        }catch (Exception e) {
-            throw new ServerErrorException("[Book Exercise] - Falha ao obter objeto \"lightBody\"");
-        }
-    }
-
-    private String buildLightAnswerResponse(JSONObject jsonObject) {
-        try{
-            return jsonObject.get("lightAnswer").toString();
-        }catch (Exception e) {
-            throw new ServerErrorException("[Book Exercise] - Falha ao obter objeto \"lightAnswer\"");
-        }
-    }
-
-    private List<String> buildLightSolutionResponse(JSONObject jsonObject) {
-        try{
-            List<String> lightSolution = new ArrayList<>();
-            JSONArray lightSolutionResponse = jsonObject.getJSONArray("lightSolution");
-
-            for(int i=0; i<lightSolutionResponse.length(); i++) {
-                lightSolution.add(lightSolutionResponse.get(i).toString());
-            }
-            return lightSolution;
-        }catch (Exception e) {
-            throw new ServerErrorException("[Book Exercise] - Falha ao obter objeto \"lightSolution\"");
-        }
     }
 }
