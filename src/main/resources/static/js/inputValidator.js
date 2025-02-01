7a0a3f46b479d511dcf24cb8b664525575722946
thomazf <thomaz.fneves@gmail.com>
2025-02-01 01:21:24 -0300

    criação inicial da resposta de teoria

--- src/main/resources/static/js/inputValidator.js
+++ src/main/resources/static/js/inputValidator.js
@@ -22,10 +22,63 @@ btnUnlock.addEventListener("click", ()=>{
         }),
         headers: {
             "Content-Type" : "application/json",
-            "Authorization" : "eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjozOTMzOTgsImdsb2JhbF91bml2ZXJzaXR5X2lkIjoiZGNiNGU1YzEtY2VjZi00MzFhLTlmNzgtYTgzM2I3ZGFlNGIyIiwiaXNfbG9nZ2VkIjp0cnVlLCJoYXNfYWNjZXNzIjpmYWxzZSwibG9naW5fdG9rZW4iOiJZckdraVphRFBXQ1RWc0pCTHU4VyIsIm1vYmlsZV90b2tlbiI6bnVsbCwiZ2xvYmFsX2NhbXB1c19pZCI6ImZmNGIwNzg1LWNmZjctNDJiZi1iMTkxLTNkNzhhNmU1N2Y2MCIsInVuaXZlcnNpdHlfaGFzX2NhbXBpIjp0cnVlLCJzZXNzaW9uX2lkIjoxMTM0OTkzMiwicGxhdGZvcm0iOiJXZWIifSwiZXhwIjoxNzM4MzA0OTg2fQ.vUaKA1Si5nPXs0njachk28l5JCwC6rpf3fmCGZngBQg"
+            "Authorization" : "eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjozOTMzOTgsImdsb2JhbF91bml2ZXJzaXR5X2lkIjoiZGNiNGU1YzEtY2VjZi00MzFhLTlmNzgtYTgzM2I3ZGFlNGIyIiwiaXNfbG9nZ2VkIjp0cnVlLCJoYXNfYWNjZXNzIjpmYWxzZSwibG9naW5fdG9rZW4iOiJZckdraVphRFBXQ1RWc0pCTHU4VyIsIm1vYmlsZV90b2tlbiI6bnVsbCwiZ2xvYmFsX2NhbXB1c19pZCI6ImZmNGIwNzg1LWNmZjctNDJiZi1iMTkxLTNkNzhhNmU1N2Y2MCIsInVuaXZlcnNpdHlfaGFzX2NhbXBpIjp0cnVlLCJzZXNzaW9uX2lkIjoxMTM0OTkzMiwicGxhdGZvcm0iOiJXZWIifSwiZXhwIjoxNzM4Mzk5NTIwfQ.Nyh8RFXvR3dFPxR6MNRNToaSESqbPFLZ2ar4dQdsFMU"
         }
     }).then((resp) => {
-        //TODO: LOGICA Request Sucesso
+        if (resp.data.resourceType == "video") {
+            //TODO: IMPLEMENTAÇÃO AULÃO
+            return;
+        }
+        if (resp.data.resourceType == "book_exercise") {
+            //TODO: IMPLEMENTAÇÃO BOOK EXERCISE
+            return;
+        }
+        if (resp.data.resourceType == "exercise") {
+            //TODO: IMPLEMENTAÇÃO BOOK EXERCISE
+            return;
+        }
+        if (resp.data.resourceType == "list_exercise") {
+            //TODO: IMPLEMENTAÇÃO BOOK LIST_EXERCISE
+            return;
+        }
+        if (resp.data.resourceType == "theory") {
+            const SINGLE_VIDEO_SIZE = 450;
+            const SPACE_BETWEEN_VIDEOS = 50;
+            let totalHeight = (SINGLE_VIDEO_SIZE*resp.data.videos.length) + (SPACE_BETWEEN_VIDEOS*resp.data.videos.length);
+            let content = ``;
+
+            content = `
+                <div class="row">
+                     <div class="col-12">
+                         ${resp.data.lightBody}
+                     </div>
+                 </div>
+             `;
+
+             content += `
+                <div class="row" style="height: 600px !important;">
+                     <div class="col-12">
+                         <h1 style="color: #00b8d6; font-size: 1.7em; font-family:Droid Serif, serif; font-weight: inherit; margin: 50px 0px 30px 0px">Vídeo Aula</h1>
+             `;
+
+             (resp.data.videos).forEach((video) => {
+                content += (video.provider.includes("youtube"))
+                    ? `<div data-cy="video-iframe" allowfullscreen="" frameborder="0" style="width: 100%; height: ${SINGLE_VIDEO_SIZE}px;">
+                           <div style="width: 100%; height: 100%;">
+                               <iframe frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="100%" height="100%" src="https://www.youtube.com/embed/${video.providerId}?autoplay=0&amp;mute=0&amp;controls=1&amp;origin=https%3A%2F%2Fapp.respondeai.com.br&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=1"></iframe>
+                           </div>
+                       </div>
+                       <div style="height: ${SPACE_BETWEEN_VIDEOS}px !important"></div>`
+
+                    : `<div style="padding:56.25% 0 0 0;position:relative;">
+                         <iframe src="https://player.vimeo.com/video/${video.providerId}" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                       </div>
+                       <div style="height: ${SPACE_BETWEEN_VIDEOS}px !important"></div>`;
+             });
+            content += `</div></div>`;
+            cardContent.innerHTML = content;
+            return;
+        }
     }).catch((erro) => {
         if (erro.response.status == 401) {
             setInvalidOrExpiredTokenBlock();
