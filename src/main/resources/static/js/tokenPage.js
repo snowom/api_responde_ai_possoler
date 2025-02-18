e7b08a48aafe1dfad29768aa8e79a5549f5d9bd0
thomazf <thomaz.fneves@gmail.com>
2025-02-18 00:35:18 -0300

    adicionado prismjs para code snippet

--- src/main/resources/static/js/tokenPage.js
+++ src/main/resources/static/js/tokenPage.js
@@ -1,6 +1,8 @@
 let btnSetToken = document.getElementById("btn_set_token");
 let inputToken = document.getElementById("input_token");
 let cardContent = document.getElementById("card_content");
+let btnCopy = document.getElementById("btn_copy");
+let codeBlock = document.getElementById("code_block");
 const LOCAL_STORAGE_KEY = "token_respai";
 
 window.addEventListener('load', ()=>{
@@ -9,6 +11,21 @@ window.addEventListener('load', ()=>{
         return;
     }
 })
+
+btnCopy.addEventListener("mouseover", ()=> {
+    btnCopy.style.background = "transparent";
+}, false);
+
+btnCopy.addEventListener("click", ()=> {
+    try{
+        codeBlock.select();
+        document.execCommand("copy");
+        setSuccessCopyIcon();
+    }catch(error){
+        console.error(error);
+    }
+}, false);
+
 
 btnSetToken.addEventListener("click", ()=> {
     btnSetToken.disabled = true;
@@ -71,4 +88,11 @@ function setErrorResponseBlock(errorMessage) {
              </div>
          </div>
      `;
+}
+
+function setSuccessCopyIcon() {
+    btnCopy.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #6c757d;transform: ;msFilter:;">
+            <path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path>
+        </svg>`;
 }
