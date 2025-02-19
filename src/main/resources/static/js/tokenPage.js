fdd0d3a11f9d1165c20760527716d2bd0365a224
Thomaz Ferreira <70149250+snowom@users.noreply.github.com>
2025-02-19 00:17:51 -0300

    Enhancement/snippet codigo js token (#18)
    * adicionado prismjs para code snippet
    
    * finallizado pagina snippet token

--- src/main/resources/static/js/tokenPage.js
+++ src/main/resources/static/js/tokenPage.js
@@ -1,7 +1,11 @@
 let btnSetToken = document.getElementById("btn_set_token");
 let inputToken = document.getElementById("input_token");
 let cardContent = document.getElementById("card_content");
+let btnCopy = document.getElementById("btn_copy");
+let codeBlock = document.getElementById("code_block");
 const LOCAL_STORAGE_KEY = "token_respai";
+
+setDefaultValuesInPrismPlugin();
 
 window.addEventListener('load', ()=>{
     if(localStorage.getItem(LOCAL_STORAGE_KEY) != "") {
@@ -9,6 +13,23 @@ window.addEventListener('load', ()=>{
         return;
     }
 })
+
+btnCopy.addEventListener("mouseover", ()=> {
+    btnCopy.style.background = "transparent";
+}, false);
+
+btnCopy.addEventListener("click", ()=> {
+    try{
+        btnCopy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #6c757d;transform: ;msFilter:;"><path d="M20 2H10c-1.103 0-2 .897-2 2v4H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2v-4h4c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM4 20V10h10l.002 10H4zm16-6h-4v-4c0-1.103-.897-2-2-2h-4V4h10v10z"></path><path d="M6 12h6v2H6zm0 4h6v2H6z"></path></svg>`;
+        let code = codeBlock.getAttribute('value');
+        navigator.clipboard.writeText(code).then(()=>{
+            setSuccessCopyIcon();
+        });
+    }catch(error){
+        console.error(error);
+    }
+}, false);
+
 
 btnSetToken.addEventListener("click", ()=> {
     btnSetToken.disabled = true;
@@ -71,4 +92,21 @@ function setErrorResponseBlock(errorMessage) {
              </div>
          </div>
      `;
+}
+
+function setSuccessCopyIcon() {
+    btnCopy.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #6c757d;transform: ;msFilter:;">
+            <path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path>
+        </svg>`;
+}
+
+function setDefaultValuesInPrismPlugin() {
+    Prism.plugins.NormalizeWhitespace.setDefaults({
+        'remove-trailing': true,
+        'remove-indent': true,
+        'left-trim': true,
+        'right-trim': true,
+        'break-lines': 60, //max number of characters in each line before break
+    });
 }
